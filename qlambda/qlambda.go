5075604e3b91e13bfbeae3c85878752a67634afc
Vedran Stankovic <contact@vedrans.me>
2016-10-14 20:11:41 +0100

    changed errorresponse to koresponse

--- qlambda/qlambda.go
+++ qlambda/qlambda.go
@@ -2,13 +2,13 @@ package qlambda
 
 import "net/http"
 
-// ErrorResponse prepares response when error happens
+// KOResponse prepares response when error happens
 // accepts error, returns body and status code that
 // that should be passed to clent
-func ErrorResponse(err error) (body map[string]interface{}, statusCode int) {
+func KOResponse(reason string) (body map[string]interface{}, statusCode int) {
 	body = make(map[string]interface{})
 	body["status"] = "ko"
-	body["reason"] = err.Error
+	body["reason"] = reason
 	statusCode = http.StatusBadRequest
 	return body, statusCode
 
