8f097a9330cfe31276fd4917c8ac304e4d971f9e
Vedran Stankovic <contact@vedrans.me>
2016-10-24 19:47:21 +0100

    added helpers for getting req params

---
+++ qlambda/requests.go
@@ -0,0 +1,76 @@
+package qlambda
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/vsrc/nile/qlambda"
+)
+
+func GetReqHeaders(event map[string]interface{}, headers ...string) (map[string]interface{}, error) {
+
+	var resp map[string]interface{}
+
+	reqheaders, err := IfEventParamOK(event, "headers", "Request Headers")
+	if err != nil {
+		return resp, nil
+	}
+
+	for _, name := range headers {
+		value, err := IfReqParamEmptyOrMissing(reqheaders, name)
+		if err != nil {
+			return resp, err
+		}
+		resp[name] = value
+	}
+
+	return resp, nil
+
+}
+
+func GetReqBodyParam(event map[string]interface{}, params ...string) (map[string]interface{}, error) {
+	var resp, reqbody map[string]interface{}
+
+	bodystr, ok := event["body"].(string)
+	if !ok {
+		return resp, errors.New("Invalid request! Body is missing.")
+	}
+	if err := json.Unmarshal([]byte(bodystr), &reqbody); err != nil {
+		return resp, err
+	}
+
+	for _, name := range params {
+		value, err := IfReqParamEmptyOrMissing(reqbody, name)
+		if err != nil {
+			return resp, err
+		}
+		resp[name] = value
+	}
+
+	return resp, nil
+
+}
+
+func GetReqIdentityParam(event map[string]interface{}, params ...string) (map[string]interface{}, error) {
+	var resp map[string]interface{}
+
+	reqctx, err := qlambda.IfEventParamOK(event, "requestContext", "Request Context")
+	if err != nil {
+		return resp, err
+	}
+	reqid, err := qlambda.IfEventParamOK(reqctx, "identity", "Request Identity")
+	if err != nil {
+		return resp, err
+	}
+
+	for _, name := range params {
+		value, err := IfReqParamEmptyOrMissing(reqid, name)
+		if err != nil {
+			return resp, err
+		}
+		resp[name] = value
+	}
+
+	return resp, nil
+
+}
